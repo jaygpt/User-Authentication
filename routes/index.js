b71410ee5280cc6d053a70c395d1aecc8b02ede7
Jay <jaygpt@iitk.ac.in>
2018-05-12 03:24:47 +0530

    first Lecture

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/',function(req,res){
+router.get('/',ensureAuthentication,function(req,res){
     res.render('index');
 });
 
+function ensureAuthentication(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    else{
+        req.flash('error_msg','You are not logged in');
+        res.redirect('/users/login');
+    }
+}
 module.exports = router;
